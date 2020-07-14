663a011e374781bc70aa13d627ebbcdc9456495b
jonathan lopez <jdavelg@gmail.com>
2020-07-13 19:00:24 -0600

    main js url fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var url = 'https://secure-meadow-51659.herokuapp.com/';
+var url = 'https://secure-meadow-51659.herokuapp.com';
 
 window.addEventListener("load", function() {
     $('.btn-like').css('cursor', 'pointer');
